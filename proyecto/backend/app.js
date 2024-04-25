4c537016532aaeb007f8f147b83e129f27a81cdb
Daniel Castro <112607909+DanielCastr02@users.noreply.github.com>
2024-04-25 10:53:37 -0700

    conexion de base de datos

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -1,20 +1,61 @@
-let express = require('express');
-let mysql = require('mysql');
-let cors = require('cors');
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
 
-let app = express();
+const app = express();
 app.use(express.json());
 app.use(cors()); //el cors sirve para aceptar solicitudes de un servidor que no es el nuestro
 
-//Rutas de acceso
-app.get('/', function(req,res){
-    res.send('Rutaaaaaa de iniciosssss');
+const conexion = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+})
 
+// Nos conectamos a MySQL
+conexion.connect((error) => {
+    if (error) {
+        throw error;
+    } else { 
+        console.log('Conectado a la base de datos');
+    }
 });
 
+//Rutas de acceso
+app.get('/', (req, res) => { 
+    res.send('Rutaaaaaa de iniciosssss');
+});   
+
+app.get('/api/clientes', (req, res) => {
+    conexion.query("SELECT * FROM clientes", (error, resultados) => {
+        if (error) {
+            return res.send(error)
+        }
+        return res.send(resultados)
+    })
+})
+
+app.get('/api/clientes/:id', (req, res) => {
+    const { id } = req.params
+    conexion.query("SELECT * FROM clientes WHERE id = ?", [id], (error, resultados) => {
+        if (error) {
+            return res.send(error)
+        }
+
+        if (resultados.length > 0) {
+            return res.send(resultados[0])
+        } 
+            
+        return res.send({
+            error: "Cliente no encontrado"
+        })
+    })
+})
 
 //Encender el servidor
 let puerto = 3000;
-app.listen(puerto,function(){
+app.listen(puerto, () => {
     console.log('Servidor Conectado a puerto:' + puerto);
 });
