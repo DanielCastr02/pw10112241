d3a546ddfb84aa3a684673c016a10eba8000a214
Daniel Castro <112607909+DanielCastr02@users.noreply.github.com>
2024-05-09 10:56:47 -0700

    HICIMOS LA VENTANA CLIENTES

---
+++ frontend/src/views/ClientesView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4> Clientes </h4>
+                <RouterLink to="/clientes/create" class="btn btn-primary float-end">
+                    Agregar
+                </RouterLink>
+            </div>
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</template>
---
+++ frontend/src/views/Menuview.vue
@@ -0,0 +1,22 @@
+<script>
+  import { RouterLink } from 'vue-router'
+</script>
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">WeroxStore</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink class="navbar-brand" to="/clientes"> Clientes </RouterLink>
+          <RouterLink class="navbar-brand" to="/proveedores"> Proveedores </RouterLink>
+          <RouterLink class="navbar-brand" to="/ventas"> Ventas </RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
