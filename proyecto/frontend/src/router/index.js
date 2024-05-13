80d1a2d579f42e17b3dbb101fe0b93c9e95f0434
Daniel Castro <112607909+DanielCastr02@users.noreply.github.com>
2024-05-13 11:06:53 -0700

    actualizacion bien

--- proyecto/frontend/src/router/index.js
+++ proyecto/frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ClientesView from '../views/ClientesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/clientes',
+      name: 'clientes',
+      component: ClientesView
     },
     {
       path: '/about',
---
+++ proyecto/frontend/src/views/ClientesView.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4> Clientes </h4>
+                <RouterLink to="/clientes/create" class="btn btn-primary float-end">
+                    Agregar
+                </RouterLink>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Direccion</th>
+                    <th>Telefono</th>
+                    <th>RFC</th>
+                    <th>CURP</th>
+                    <th>CP</th>
+                    <th>Acciones</th>
+                </thead>
+                <tbody>
+                    <tr v-for ="(cliente,index) in clientes" :key="index">
+                        <td> {{ cliente.id }}</td>
+                        <td> {{ cliente.nombre }}</td>
+                        <td> {{ cliente.direccion }}</td>
+                        <td> {{ cliente.apellido }}</td>
+                        <td> {{ cliente.telefono }}</td>
+                        <td> {{ cliente.rfc }}</td>
+                        <td> {{ cliente.curp }}</td>
+                        <td> {{ cliente.cp }}</td>
+                        <td> editar &nbsp; Borrar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+    import axios from 'axios';
+    export default {
+        name: "ClientesView",
+        data(){
+            return{
+                clientes: [],
+            }
+
+        },
+        mounted() {
+            this.getClientes()
+        },
+        methods: {
+            getClientes(){
+                axios.get('http://localhost:3000/api/clientes').then(res =>{
+                    this.clientes = res.data;
+
+                })
+            }
+
+        }
+    }
+</script>
